38cb973886700c5c50c1ea976671f0549cf80654
Stefano <divitostefano@yahoo.it>
2017-02-03 23:22:11 +0100

    IT/EN fix

--- it/sendEmail.php
+++ it/sendEmail.php
@@ -1,37 +1,41 @@
 ﻿<?
-	// EDIT THE 2 LINES BELOW AS REQUIRED
-    $email_to = "postmaster@sacommittolove.com";
-    $email_subject = "Matrimonio";
-		echo '<script>console.log("a")</script>';
+	$user_ip = getenv('REMOTE_ADDR');
+	$geo = unserialize(file_get_contents("http://www.geoplugin.net/php.gp?ip=$user_ip"));
+	$country = $geo["geoplugin_countryName"];
+	$city = $geo["geoplugin_city"];
 
-	$first_name = $_POST['name']; // required
+	function clean_string($string) {
+		$bad = array("content-type","bcc:","to:","cc:","href");
+		return str_replace($bad,"",$string);
+    }
+	
+	$first_name = $_POST['name']; 
 	$email_from = $_POST['email']; // required
 	$attending = $_POST['attending']; // not required
-    $guest = $_POST['guest']; // required
+    $guest = $_POST['guest']; 
 	$altro = $_POST['altro'];
 
-	echo '<script>console.log("'.$first_name.'")</script>';
-	if(empty($first_name) || empty($email_from)) 
-    { 
-        echo '<script> alert ("Please go back and fill in all required lines"); window.history.back()</script>';
-    }
+
+	//echo '<script>console.log("'.$first_name.'")</script>';
+	//if(empty($first_name) || empty($email_from)) 
+    //{ 
+    //    echo '<script> alert ("Please go back and fill in all required lines"); window.history.back()</script>';
+    //}
 	
 	
     $error_message = "";
     $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
 	
-	$email_message = "Form details below.\n\n";
+    $email_to = "postmaster@sacommittolove.com";
+    $email_subject = "Hai ricevuto un messaggio da ".clean_string($first_name)."!";
+		
     
-	function clean_string($string) {
-      $bad = array("content-type","bcc:","to:","cc:","href");
-      return str_replace($bad,"",$string);
-    }
-    
-	$email_message .= "First Name: ".clean_string($first_name)."\n";
+	$email_message .= "Nome e cognome: ".clean_string($first_name)."\n";
     $email_message .= "Email: ".clean_string($email_from)."\n";
-    $email_message .= "Attending: ".clean_string($attending)."\n";
-    $email_message .= "Guest: ".clean_string($guest)."\n";
-	$email_message .= "Altro: ".clean_string($altro)."\n";
+    $email_message .= "Parteciperai? ".clean_string($attending)."\n";
+    $email_message .= "Quanti sarete? ".clean_string($guest)."\n";
+	$email_message .= "Altro: ".clean_string($altro)."\n\n";
+	$email_message .= "(città: ".$city.")";
 	
 	// create email headers
 	$headers = 'From: '.$email_from."\r\n".
