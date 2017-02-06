55fd7775edbf81462d182f17f279abe3b41afb9d
Stefano <divitostefano@yahoo.it>
2017-02-06 12:19:45 +0100

    IT/EN: sendEmail.php

--- en/sendEmail.php
+++ en/sendEmail.php
@@ -1,36 +1,67 @@
 ﻿<?
-	// EDIT THE 2 LINES BELOW AS REQUIRED
-    $email_to = "postmaster@sacommittolove.com";
-    $email_subject = "Your email subject line";
-		
-	$first_name = $_POST['inputname']; // required
-	$email_from = $_POST['inputemail']; // required
-	$attending = $_POST['attending']; // not required
-    $guest = $_POST['guest']; // required
 	
-	//echo '<script>console.log("'.$first_name.'")</script>';
-	
-    $error_message = "";
-    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
-	
-	$email_message = "Form details below.\n\n";
-    
 	function clean_string($string) {
-      $bad = array("content-type","bcc:","to:","cc:","href");
-      return str_replace($bad,"",$string);
+		$bad = array("content-type","bcc:","to:","cc:","href");
+		return str_replace($bad,"",$string);
     }
-    
-	$email_message .= "First Name: ".clean_string($first_name)."\n";
-    $email_message .= "Email: ".clean_string($email_from)."\n";
-    $email_message .= "Attending: ".clean_string($attending)."\n";
-    $email_message .= "Guest: ".clean_string($guest)."\n";
 	
-	// create email headers
-	$headers = 'From: '.$email_from."\r\n".
-	@mail($email_to, $email_subject, $email_message, $headers);  
 	
+	// Recupero informazioni utente
+	$user_ip = getenv('REMOTE_ADDR');
+	$geo = unserialize(file_get_contents("http://www.geoplugin.net/php.gp?ip=$user_ip"));
+	$country = $geo["geoplugin_countryName"];
+	$city = $geo["geoplugin_city"];
+	
+	
+	// Recupero form RSVP
+	$name = $_POST['name']; 
+	$email = $_POST['email']; // required
+	$attending = $_POST['attending']; // not required
+    $guest = $_POST['guest']; 
+	$altro = $_POST['altro'];
+
+	// e-mail verso me
+    $email_to = "postmaster@sacommittolove.com";
+    $email_subject = "Hai ricevuto un messaggio da ".clean_string($name)."!";
+		
+    $email_message .= "Nome e cognome: ".clean_string($name)."\n";
+    $email_message .= "Email: ".clean_string($email)."\n";
+    $email_message .= "Parteciperai? ".clean_string($attending)."\n";
+    $email_message .= "Quanti sarete? ".clean_string($guest)."\n";
+	$email_message .= "Altro: ".clean_string($altro)."\n\n";
+	$email_message .= "(città: ".$city.")";
+	
+	$headers = 'From: '.$email."\r\n".
+	@mail($email_to, $email_subject, $email_message, $headers);
+
+
+	// E-MAIL VERSO UTENTE
+	$email_subject_2 = "Stefano e Angeni ti ringraziano per il messaggio!";
+	$email_message_2 = "";
+	$email_message_2 .= "Ciao ".clean_string($name).",\n";
+	$email_message_2 .= "\tgrazie per averci scritto. Sarà nostra cura provvedere alle tue richieste, qualora ce ne fossero (fino al 29 Aprile!).";
+	$email_message_2 .= "\nPer qualsiasi ulteriore esigenza, di preghiamo di contattarci via telefono (riportato in fondo all'invito, sperando che tu non l'abbia già buttato!)";
+	$email_message_2 .= " oppure ad una delle seguenti email:\n";
+	$email_message_2 .= "- Stefano: divitostefano@yahoo.it  oppure  divitostefano@gmail.com\n";
+	$email_message_2 .= "- Angeni: angeni.uminga@gmail.com  oppure  angenie13@hotmail.com\n";
+	
+	$email_message_2 .= "\nDi seguito trovi il messaggio che hai inviato:\n";
+	$email_message_2 .= "Nome e cognome: ".clean_string($name)."\n";
+    $email_message_2 .= "Email: ".clean_string($email)."\n";
+    $email_message_2 .= "Parteciperai? ".clean_string($attending)."\n";
+    $email_message_2 .= "Quanti sarete? ".clean_string($guest)."\n";
+	$email_message_2 .= "Altro: ".clean_string($altro)."\n\n";
+	
+	$email_message_2 .= "Grazie mille!";
+	$email_message_2 .= "\nStefano & Angeni";
+	
+	//$headers = "From: postmaster@sacommittolove.com\r\n".
+
+	@mail($email, $email_subject_2, $email_message_2);
+	
+
 	$referer = $_SERVER['HTTP_REFERER'];
-	header("Location: $referer");
+	header("Location: rsvp.html?message=ok");
 	exit;
 
 ?>
